603fd8281d5182cc737077b8ee30c451bde2b540
bocajji <jacobkwood@gmail.com>
2023-08-17 16:24:42 +0200

    Adds mock data fetch to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { ProductList } from '@/components/ProductList/ProductList';
+import { ProductMock } from '@/types/product';
 
-export default function Home() {
+const getProducts = async () => {
+  return Promise.resolve([ProductMock, ProductMock, ProductMock, ProductMock, ProductMock])
+}
+
+export default async function Home() {
+  const products = await getProducts();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    <div>
+      <ProductList products={products}/>
+    </div>
   )
 }
