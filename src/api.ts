ce2297116e7fd3da190ebf0652475b0d02171acf
bocajji <jacobkwood@gmail.com>
2023-08-22 15:07:09 +0200

    Uses fetch instead of axios

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { Product } from '@/types/product';
 
 const url = 'https://file.notion.so/f/s/38d3f633-2c6d-4652-abf0-79a782786475/products.json?id=ba896742-ed51-4c24-8e53-db630c74a208&table=block&spaceId=7974307b-04a3-44a1-9d3b-367405cb7e16&expirationTimestamp=1692777600000&signature=mFByK8lijOw_065HS7rLFHPl7v4Z8H_czwtFtTsrW-E&downloadName=products.json'
@@ -19,16 +18,18 @@ const transformProductDto = (data: any): Product => {
 		imageData: {
 			url: data.images[0].variants[300].formats.jpg.resolutions['1x'].url,
 			alt: `Image of the product ${data.name} from ${data.supplier},`
-		}
+		},
+		isInCheckout: false,
+		totalAdded: 0,
 	}
 }
 
 export const fetchProducts = async (): Promise<Product[]> => {
-	const response = await axios.get(url)
-	//fixes json
-	const rawData = `[${response.data}]`;
+	const response = await fetch(url)
+	const rawData = await response.text();
+	const fixedData = `[${rawData}]`;
 	try {
-		const data = JSON.parse(rawData);
+		const data = JSON.parse(fixedData);
 		return data.map(transformProductDto);
 	} catch (err) {
 		console.log(err)
