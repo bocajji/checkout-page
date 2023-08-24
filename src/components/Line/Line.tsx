aae41a36136efa0a74cb3dfa5248bc546faef6c4
bocajji <jacobkwood@gmail.com>
2023-08-24 16:21:46 +0200

    Fixes line component display

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 
 interface Props {
 	pixelHeight?: number
 }
 
 export const Line = ({ pixelHeight = 1 }: Props) => {
-	const height = `h-[${pixelHeight}px]`
+	const [height, setHeight] = useState<number | undefined>(1);
+	useEffect(() => {
+		setHeight(pixelHeight)
+	}, [pixelHeight])
 	return (
-		<div className={`bg-borderLight w-full inline-block ${height}`} />
+		<div className={`bg-borderLight w-full inline-block h-[${height}px]`} />
 	)
 }
