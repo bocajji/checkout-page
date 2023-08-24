b9d7aa3ac070400da28319320ea8001e793758cf
bocajji <jacobkwood@gmail.com>
2023-08-24 17:34:47 +0200

    Css cleanup on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,9 @@ export default async function Home() {
   }
   return (
     <ProductCheckoutProvider productList={products}>
-      <div className="w-full flex flex-col large:flex-row">
-        <div className="relative px-4 pt-4 large:px-74 large:pt-74">
+      <div className="w-full flex flex-col large:flex-row large:p-74">
           <ProductList />
-        </div>
-        <div className="relative z-10 large:flex-grow-1 large:p-74">
           <CheckoutCart />
-        </div>
       </div>
     </ProductCheckoutProvider>
   )
